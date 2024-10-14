8b56011fb3d0a13d721d91fa36d0a48ad7622385
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-10-14 15:43:40 -0400

    Security config class

--- src/main/java/com/example/cloud/api/SecurityConfig.java
+++ src/main/java/com/example/cloud/api/SecurityConfig.java
@@ -1,2 +1,49 @@
-package com.example.cloud.api;public class SecurityConfig {
+package com.example.cloud.api;
+
+import com.example.cloud.api.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/v2/user").permitAll()
+                        .requestMatchers("/healthz").permitAll()
+                        .requestMatchers("/v2/user/verify").permitAll()
+                        .requestMatchers("/v2/user/self").authenticated()
+
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12); //when a user logs in, SpBoot will encode with this same encoder
+        //and compare the encoded with one stored in database (also should be same if password is correct)
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
 }
